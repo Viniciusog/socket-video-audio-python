39d2eb3fd4b16bed1af09f731aa2daaba044da92
viniciusog <viniciusogbr@gmail.com>
2023-07-02 01:25:20 -0300

    Comunicação com texto e vídeo funcionando

--- main.py
+++ main.py
@@ -9,6 +9,19 @@ import tkinter
 import cv2
 import base64
 import numpy as np
+import socket
+
+text_port = 6000
+video_port = 6001
+audio_port = 6002
+
+def get_local_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    # Tenta se conectar a um servidor DNS
+    s.connect(('8.8.8.8', 80))
+    # Obtém o endereço IP local do socket
+    local_ip = s.getsockname()[0]
+    return local_ip
 
 # criar função para pub e função para sub
 # cada uma dessas funções será executada em uma thread diferente (para ser multithread)
@@ -17,28 +30,33 @@ def pub_function(port_pub, zmq_context):
     print("Conectando pub_text na porta %s\n" % port_pub)
     socket.bind("tcp://*:%s" % port_pub)
 
-    while True:
+    """ while True:
         topic = random.randrange(9999,10005)
         topic = "*"
         messagedata = random.randrange(1,215) - 80
         print("%s %d" % (topic, messagedata))
         socket.send(b"%s %d" % (topic.encode(), messagedata))
-        time.sleep(1)
+        time.sleep(1) """
+    
+    topic = "*" + get_local_ip()
+    while True:
+        message = input()
+        socket.send(b"%s %s" % (topic.encode(), message.encode()))
 
-def sub_function(port_pub, array_ports_connect, zmq_context):
+def sub_function(port_pub, ips_to_connect, zmq_context):
     socket = zmq_context.socket(zmq.SUB)
-    for port_to_connect in array_ports_connect:
-        socket.connect("tcp://localhost:%s" % int(port_to_connect))
+    for ip in ips_to_connect:
+        socket.connect("tcp://%s:%s" % (ip, int(text_port)))
+        socket.subscribe("*" + ip)
     # Aqui estamos nos inscrevendo em todos os conteúdos que tiverem a nossa porta
     # pois assim, saberemos que a mensagem é para a gente
-    socket.subscribe(port_pub)
+    #socket.subscribe(str(port_pub))
     # Aqui estamos nos inscrevendo no conteúdo que começar com *
-    socket.subscribe("*")
-
+    
     while True:
         string = socket.recv()
-        topic, messagedata = string.split()
-        print("Mensagem recebida: %s, tópico: %s\n" % (messagedata, topic))
+        topic, messagedata = string.split(b" ", 1)
+        print("%s: %s\n" % (topic.decode(), messagedata.decode()))
 
 def pub_video(port_pub, zmq_context):
     socket = zmq_context.socket(zmq.PUB)
@@ -51,7 +69,7 @@ def pub_video(port_pub, zmq_context):
     while True:
         (grabbed, frame) = camera.read()
         encoded, buffer = cv2.imencode('.jpg', frame)
-        topic = "*" + port_pub
+        topic = "*" + get_local_ip()
 
         to_send = b'%s ' % (topic.encode())
         to_send += base64.b64encode(buffer)
@@ -64,83 +82,63 @@ def pub_video(port_pub, zmq_context):
     camera.release()
     cv2.destroyAllWindows()
 
-def sub_video(port_pub, array_ports_connect, zmq_context):
+def sub_video(port_pub, ips_to_connect, zmq_context):
     socket = zmq_context.socket(zmq.SUB)
     topics_to_subscribe = []
 
-    for port_to_connect in array_ports_connect:
-        socket.connect("tcp://localhost:%s" % int(port_to_connect))
-        topic = "*" + port_to_connect
+    for ip in ips_to_connect:
+        socket.connect("tcp://%s:%s" % (ip, int(video_port)))
+        topic = "*" + ip
         topics_to_subscribe.append(topic)
     
-    socket.subscribe(port_pub)
+    socket.subscribe("*")
 
     for topic_to_subscribe in topics_to_subscribe:
-        print("\nSubscribing to %s\n" % (topic_to_subscribe))
+        print("Subscribing to %s\n" % (topic_to_subscribe))
         socket.subscribe(topic_to_subscribe)
 
     while True:
         try:
             string = socket.recv()
             topic, frame_encoded = string.split()
-            print("Vídeo recebido: %s, tópico: %s\n" % (frame_encoded, topic))
             img = base64.b64decode(frame_encoded)
-            npimg = np.fromstring(img, dtype=np.uint8)
+            npimg = np.frombuffer(img, dtype=np.uint8)
             source = cv2.imdecode(npimg, 1)
-            cv2.imshow(topic, source)
+            cv2.imshow(str(topic.decode()), source)
             cv2.waitKey(1)
         except KeyboardInterrupt:
             cv2.destroyWindow()
             break
 
+strArgv = ""
+for element in sys.argv[1:]:
+    strArgv += str(element) + " "
+strArgv = strArgv.strip()
 
-indiceS1 = sys.argv.index('-')
-portas_pubs = sys.argv[1:indiceS1]
-resto = sys.argv[indiceS1+1:]
+print(":%s:" % (strArgv))
 
-indiceS2 = resto.index('-')
-text_ports_to_connect = resto[0:indiceS2]
-resto2 = resto[indiceS2+1:]
+nodes = strArgv.split("-node ")
+for i in range(1, len(nodes)-1):
+    nodes[i] = nodes[i].strip()
 
-indiceS3 = resto2.index('-')
-video_ports_to_connect = resto2[0:indiceS3]
-resto3 = resto2[indiceS3+1:]
-
-audio_ports_to_connect = resto3
-
-print(portas_pubs)
-print(text_ports_to_connect)
-print(video_ports_to_connect)
-print(audio_ports_to_connect)
+type_of_execution = nodes[0] # se tiver -sub, os pubs não vão executar
+print("Tipo de execução: %s:" % (type_of_execution))
+nodes = nodes[1:]
+print(nodes)
 
 # Socket to talk to server
 context = zmq.Context()
 
-thread_pub = threading.Thread(target=pub_function, args=(portas_pubs[0], context))
-thread_pub.start()
+if type_of_execution != "-sub ":
+    thread_pub = threading.Thread(target=pub_function, args=(text_port, context))
+    thread_pub.start()
 
-thread_sub = threading.Thread(target=sub_function, args=(portas_pubs[0], text_ports_to_connect, context))
+thread_sub = threading.Thread(target=sub_function, args=(text_port, nodes, context))
 thread_sub.start()
 
-thread__pub_video = threading.Thread(target=pub_video, args=(portas_pubs[1], context))
-thread__pub_video.start()
+if type_of_execution != "-sub ":
+    thread__pub_video = threading.Thread(target=pub_video, args=(video_port, context))
+    thread__pub_video.start()
 
-thread__pub_video = threading.Thread(target=sub_video, args=(portas_pubs[1], video_ports_to_connect, context))
-thread__pub_video.start()
-
-""" print("Subscribing to ports....")
-for i in ports_subscribe:
-    print("Subscribing to " + i)
-    socketSub.connect("tcp://localhost:%s" % int(i)) """
-
-# Subscribes to all topics you can selectively create multiple workers
-# that would be responsible for reading from one or more predefined topics
-# if you have used AWS SNS this is a simliar concept
-
-""" socketSub.subscribe("*") # se o tópico da mensagem for público para todos que estiverem ouvindo
-socketSub.subscribe("")
-socketSub.subscribe(port_pub) # se o tópico da mensagem for pra mim (minha porta)
-
-while True:
-    # Receives a string format message
-    print(socketSub.recv()) """
+thread__sub_video = threading.Thread(target=sub_video, args=(video_port, nodes, context))
+thread__sub_video.start()
